8c2b44971b1454569c7249ac97c9580ed127a557
Somethingcat7 <106313467+HeinKroese@users.noreply.github.com>
2022-10-19 15:46:02 +0200

    Added Items, Gold and start of mage

---
+++ GADE6122-POE/GADE6122-POE/Classes/Characters/Mage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GADE6122_POE.Characters;
+
+namespace GADE6122_POE.Classes.Characters
+{
+   class Mage : Enemy
+    {   //Constructor of Mage
+        public Mage(int X, int Y) : base(X, Y, 'M', 5, 5)
+        {
+
+        }
+
+
+    }
+}
---
+++ GADE6122-POE/GADE6122-POE/Classes/Items/Gold.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GADE6122_POE.Classes.Items
+{
+    class Gold : Item
+    {
+        private int GoldAmount;
+        private Random Randomamount = new Random();
+       
+        //Accessor for gold
+        public int MaxGoldAmount { get { return GoldAmount; } set { GoldAmount = value; } }
+        
+        //Constructor for gold
+        public Gold(int X, int Y) : base (X,Y,'G')
+        {
+            GoldAmount = Randomamount.Next(1,6);
+        }
+       
+        //String Override method
+        public override string ToString()
+        {
+            return $"Gold located at [{this.X},{this.Y}]";
+        }
+    }
+}
